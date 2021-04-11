e03f6a2b7c12a81cdc49c8864929582948106960
khannavijay <tkhannavijay@gmail.com>
2021-04-11 15:31:36 +0530

    invoice add table

--- invoice.js
+++ invoice.js
@@ -22,3 +22,40 @@
         //    alert(cellVal);
         }
  }
+
+
+ function addToTable(){
+       let catogary =  document.getElementById("categoryDataList").value;
+       let item = document.getElementById("itemDataList").value;
+       let size = document.getElementById("sizeDataList").value;
+       let qty = document.getElementById("inputQty").value;
+       let price = document.getElementById("inputPrice").value;
+       let amount = qty*price;
+       let ItemRemarks = document.getElementById("ItemRemarks").value;
+
+       let invoiceBody = document.getElementById("invoiceBody");
+
+       rowLength = oTable.rows.length;
+       console.log(rowLength);
+       
+       var row = invoiceBody.insertRow(-1);
+       var cell1 = row.insertCell(0);
+       var cell2 = row.insertCell(1);
+       var cell3 = row.insertCell(2);
+       var cell4 = row.insertCell(3);
+       var cell5 = row.insertCell(4);
+       var cell6 = row.insertCell(5);
+       var cell7 = row.insertCell(6);
+       var cell8 = row.insertCell(7);
+
+       cell1.innerHTML = rowLength;
+       cell2.innerHTML = catogary;       
+       cell3.innerHTML = item;
+       cell4.innerHTML = size; 
+       cell5.innerHTML = qty;
+       cell6.innerHTML = price; 
+       cell7.innerHTML = amount;
+       cell8.innerHTML = ItemRemarks; 
+
+       document.getElementById("entryForm").reset();
+ }
